0ca3e051bf1108e8fb5a5910a1524c207d8b6a1d
OpDragon26 <25opdragon25@gmail.com>
2025-01-08 17:06:39 +0100

    level 1 finished

--- levels.py
+++ levels.py
@@ -2,8 +2,9 @@ import clgame
 
 wall = clgame.Texture("textures/wall.txt")
 door = clgame.Texture("textures/door.txt")
+door_open = clgame.Texture("textures/door_open.txt")
 lever = clgame.Texture("textures/lever.txt")
-leverOn = clgame.Texture("textures/lever_on.txt")
+lever_on = clgame.Texture("textures/lever_on.txt")
 player =  clgame.Text("P",(0,20))
 
 
@@ -15,9 +16,27 @@ level1.addTexture(wall,"wall2",(35,0))
 level1.addTexture(door, "door1",(20,27))
 level1.addTexture(door, "door2",(35,10))
 
+level1.addTexture(door_open, "door1_open",(20,27))
+level1.addTexture(door_open, "door2_open",(35,10))
+level1.hideTexture("door1_open")
+
+level1.addTexture(lever,"lever1",(10,32))
+level1.addTexture(lever_on,"lever1_on",(10,32))
+
+level1.addTexture(lever,"lever2",(27,32))
+level1.addTexture(lever_on,"lever2_on",(27,32))
+
 level1.addText(player,"player")
 
 level1.addHitbox(clgame.Hitbox((20,0),(2,39),["wall"]),"wall1")
 level1.addHitbox(clgame.Hitbox((35,0),(2,39),["wall"]),"wall2")
 
-levels = [level1]
+level1.addHitbox(clgame.Hitbox((20,27),(2,2),["door"]),"door1")
+level1.addHitbox(clgame.Hitbox((35,10),(2,2),["door"]),"door2")
+
+level1.addHitbox(clgame.Hitbox((9,31),(3,3),["lever"]),"lever1")
+level1.addHitbox(clgame.Hitbox((26,31),(3,3),["lever"]),"lever2")
+
+level1.addHitbox(clgame.Hitbox((60,0),(2,39)),"finish")
+
+levels = [level1] 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import clgame
+import timer
 import levels
 from pynput.keyboard import *
 
@@ -8,7 +9,15 @@ levels.level1.update()
 
 playerPos = [0,20]
 
+timer = timer.Timer()
+timer.startTimer()
+
+class v1:
+    doors = True
+    spacePressed = 0
+
 def on_press(key):
+    global v1, timer, level
     if key == Key.right:
         playerPos[0] += 1
 
@@ -59,9 +68,37 @@ def on_press(key):
                 break
         if collide:
             playerPos[1] += 1
-        
+
+    elif key == Key.space:
+        if timer.getTimer() - 1 > v1.spacePressed:
+            v1.spacePressed = timer.getTimer()
+            if level == 0:
+                for hitbox in levels.levels[level].collideHitboxes(playerPos):
+                    if "lever" in hitbox.tags:
+                        v1.doors = not v1.doors
 
     elif key == Key.esc: return False
+
+    if levels.levels[level].hitboxes["finish"].collidePoint(playerPos):
+        level = 1
+        plyerPos = [0,20]
+
+    if level == 0:
+        levels.levels[level].textures["door1"].shown = v1.doors
+        levels.levels[level].textures["door2"].shown = not v1.doors
+
+        levels.levels[level].textures["door1_open"].shown = not v1.doors
+        levels.levels[level].textures["door2_open"].shown = v1.doors
+
+        levels.levels[level].textures["lever1"].shown = not v1.doors
+        levels.levels[level].textures["lever1_on"].shown = v1.doors
+
+        levels.levels[level].textures["lever2"].shown = not v1.doors
+        levels.levels[level].textures["lever2_on"].shown = v1.doors
+
+        levels.levels[level].hitboxes["door1"].active = not v1.doors
+        levels.levels[level].hitboxes["door2"].active = v1.doors
+
     levels.levels[level].moveText("player",(playerPos[0] * 2 + 1, playerPos[1]))
     levels.levels[level].update()
 
