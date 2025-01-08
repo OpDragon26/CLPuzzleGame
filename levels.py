61bfbf79c85dd172657828081279ce3af79a5778
OpDragon26 <25opdragon25@gmail.com>
2025-01-08 16:23:33 +0100

    doors

--- levels.py
+++ levels.py
@@ -1,6 +1,7 @@
 import clgame
 
-wall = clgame.Texture("textures/wall.txt") # Hitbox height: 
+wall = clgame.Texture("textures/wall.txt")
+door = clgame.Texture("textures/door.txt")
 lever = clgame.Texture("textures/lever.txt")
 leverOn = clgame.Texture("textures/lever_on.txt")
 player =  clgame.Text("P",(0,20))
@@ -10,7 +11,13 @@ level1 = clgame.Scene(texture=clgame.Texture("level_empty.txt"))
 
 level1.addTexture(wall,"wall1",(20,0))
 level1.addTexture(wall,"wall2",(35,0))
+
+level1.addTexture(door, "door1",(20,27))
+level1.addTexture(door, "door2",(35,10))
+
 level1.addText(player,"player")
+
 level1.addHitbox(clgame.Hitbox((20,0),(2,39),["wall"]),"wall1")
+level1.addHitbox(clgame.Hitbox((35,0),(2,39),["wall"]),"wall2")
 
 levels = [level1]
